0288da14abf8dc17daf5725f7dd68545af4e3b33
최권진 <rnjswls060@naver.com>
2025-04-10 18:51:39 +0900

    vercel 배포 중 오류 해결용 커밋

--- src/Components/items/ProductList.jsx
+++ src/Components/items/ProductList.jsx
@@ -6,7 +6,7 @@ const ProductList = ({ products }) => {
       {products.map((product) => (
         <div
           key={product.id}
-          className="mt-[16px] w-[168px] tablet:w-[221px] pc:w-[220px]"
+          className="mt-[16px] w-[168px] tablet:w-[221px] pc:w-[220px] "
         >
           <ProductCard cardType="All" product={product} />
         </div>
